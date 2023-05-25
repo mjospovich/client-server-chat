444e93c2e6ec5535431996dd6f21e1a31d1f437d
mjospovich <martin.josipovic2@gmail.com>
2023-05-25 19:59:35 +0200

    PID success report doesn't show up to user anymore

--- sysConfig/closeChat.bat
+++ sysConfig/closeChat.bat
@@ -4,5 +4,5 @@
 REM Description: Closes the cmd window named Chat for the user
 REM no report is generated
 
-taskkill /f /fi "WINDOWTITLE eq Chat*"
-taskkill /f /fi "WINDOWTITLE eq Chat*"
+taskkill /F /FI "WINDOWTITLE eq Chat*" >nul 2>&1
+taskkill /F /FI "WINDOWTITLE eq Chat*" >nul 2>&1
